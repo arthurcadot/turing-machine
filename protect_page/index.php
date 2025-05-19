8ba2660ae38be02e9fc4654999186da723738876
arthur <arthurkdo@gmail.com>
2025-05-19 16:49:25 +0200

    refactor admin en client sur la page de connection client

--- protect_page/index.php
+++ protect_page/index.php
@@ -9,8 +9,8 @@ setcookie('mdp_cookie_client', '', time() - 3600, "/", "", false, true);
     <link rel="stylesheet" href="../../assets/css/style.css" />
 </head>
 <body>
-    <form action="secret.php" method="post" class="container container-admin">
-        <h1>Connection a Admin</h1>
+    <form action="secret.php" method="post" class="container container-client">
+        <h1>Connection a Client</h1>
         <p>Entrez votre mot de passe :</p>
         <input class="btn no-upercase"  type="password" name="mot_de_passe" maxlength="7" placeholder="Password" autocomplete="off" style="text-align: center;width: calc(100% - 32px);">
         <button class="btn btn-blue" type="submit">Valider</button>
